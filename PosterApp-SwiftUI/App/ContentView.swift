999b76e9ad3a663c6a30ed05ebfd752e060164c1
hakkicansengonul <coder.9734@gmail.com>
2022-06-11 14:24:19 +0300

    Added animation,image Scale, image Offset

--- PosterApp-SwiftUI/App/ContentView.swift
+++ PosterApp-SwiftUI/App/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    @State private var isAnimating : Bool = false
+    @State private var imageScale : CGFloat = 1
+    @State private var imageOffset : CGSize = .zero
+    // MARK: - FUNCTIONS
+    func resetImageState(){
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    // MARK: - BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        NavigationView {
+            ZStack {
+                Image("Poster-1")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            resetImageState()
+                        }
+                    }
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
+                
+                
+            }//ZStack
+            .navigationTitle("Poster App")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.easeOut(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        }//NAVIGATION
+        
+        
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
