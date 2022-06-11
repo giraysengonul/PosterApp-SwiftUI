e1a3000406111f94516279f8c56b3ca7fc0b766d
hakkicansengonul <coder.9734@gmail.com>
2022-06-11 13:52:43 +0300

    Initial Commit

---
+++ PosterApp-SwiftUI/PosterApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosterApp_SwiftUIApp.swift
+//  PosterApp-SwiftUI
+//
+//  Created by hakkı can şengönül on 11.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PosterApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
